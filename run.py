83fc5d7646284ea18a142d25f0530452152171dc
Sachith Wickramasinghe <37221297+msachithpw@users.noreply.github.com>
2025-08-24 22:00:58 -0500

    update run
    Added error handling for file removal and improved directory management for client setup.

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import os
 import sys
 import numpy as np
 import time
+import random
+from operator import attrgetter
+from numpy.random import normal as gran
+from scipy.fft import fft, ifft
+
 RED = '\033[31m'
 GREEN = '\033[32m'
 RESET = '\033[0m'
@@ -10,10 +15,6 @@ from funcLM import param
 param = param()  # Initialize parameters
 N = param.nk
 foldprefix = "run-"
-cleanFolder = "DFTB_clean"
-
-os.system("rm -rf *.txt")  # Clean up previous run files
-os.system(f"rm -rf {foldprefix}*")
 
 os.system(f"sbatch run_server.sbatch {N}")  # Start the server
 # green color
@@ -22,16 +23,21 @@ print(f"{GREEN}Server started with {N} clients.{RESET}")
 time.sleep(30)  # Wait for the server to start
 
 
-
-
 for i in range(N):
     
-    
-    os.system(f"mkdir -p {foldprefix}{i}")  # Create a directory for each client
-    os.system(f"cp -r {cleanFolder}/* {foldprefix}{i}/")  # Copy the clean folder contents to each client's directory
-    os.system(f"cp run_client.sbatch {foldprefix}{i}/")  # Copy the run_client.sbatch file to each client's directory
-    
     os.chdir(foldprefix + str(i))  # Change to the client's directory
+
+    for fname in ["qt.out", "WaterMD_Cavity.xyz"]:
+        try:
+            os.remove(fname)
+        except FileNotFoundError:
+            pass  # Ignore if file doesn't exist
+
+
     os.system(f"sbatch run_client.sbatch {i}")  # Start each client with its index
     print(f"{RED}Client {i} started.{RESET}")
     os.chdir("..")  # Change back to the parent directory
+
+
+
+
